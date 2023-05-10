f3cb4d805d11f7152089635df68ba593ff7eae24
skumarscse-skillsafari <sathish.kumar@skillsafari.in>
2023-05-10 21:38:59 +0530

    react-intro

---
+++ React/react-intro/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+
+      <h2>Welcome, React...</h2>
+      <p>This is a paragraph</p>
+      <a href="#">Click here...</a>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/react-intro/src/Navbar.js
@@ -0,0 +1,24 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div>
+      <ul className="list">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Projects</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
